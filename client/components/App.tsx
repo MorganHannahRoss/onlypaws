b58ab6f4f846a7cd445c34f9ee2b55860aa780ac
Morgan Ross <morgan.ross@developersinstitute.co.nz>
2024-03-01 11:18:27 +1300

    Update app.tsx

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import { React, useEffect, useState } from 'react'
 // import '../main.css' // havent create yet ( CSS FILE NEEDED)
-import "../Data"
-import "../img"
+import '../Data'
+import '../img'
 
 interface Cat {
   name: string
@@ -18,7 +18,7 @@ interface Cat {
 // "personality": "Laid-back and friendly, enjoys lounging around in cozy spots.",
 // "photo": "Maine-Coon.png"
 
-const 
+const cat
 
 function App() {
   return (
@@ -28,9 +28,11 @@ function App() {
           src="{/images/color_earth.gif}" // image calling function
           alt="alt code fuction call " // ALT TEXT LINK
         />
-        <h2>{cat.name</h2> {/* name of cat in H2 */}
+        <h2>{cat.name}</h2> {/* name of cat in H2 */}
         <h3>{cat.personality}</h3> {/* descrption of cat in H3 */}
       </div>
+      {/* 
+// will map this  */}
 
       {/* <div >
         <Nav />
@@ -42,4 +44,3 @@ NOTES FOR LATER
 }
 
 export default App
-
